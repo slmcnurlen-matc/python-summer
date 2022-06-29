aa5e6b99446f43671d91f5ce0bdf609d44ac9579
Sam McNurlen <slmcnurlen@madisoncollege.edu>
2022-06-29 17:51:48 -0500

    My First Script

---
+++ week2/first-script.py
@@ -0,0 +1,5 @@
+#Author: Sam McNurlen
+#Description: This script is my first Python script and will output some information I am learning in class.
+myName = 'Sam McNurlen'
+print(myName)
+print("I learned how to format strings in Python.")
