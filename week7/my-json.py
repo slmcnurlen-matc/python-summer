8914fcdf39ab6500e1cbcb3f890774e592ff8bd8
Sam McNurlen <slmcnurlen@madisoncollege.edu>
2022-08-04 18:53:14 -0500

    JSON

--- week7/my-json.py
+++ week7/my-json.py
@@ -6,8 +6,9 @@ This script uses the json module to translate JSON into a Python dictionary.
 
 import json,requests,argparse
 parser = argparse.ArgumentParser(description='Creating a parser to add arguments')
-parser.add_argument('--ipaddress')
-myJSON = requests.get("https://ipinfo.io/52.54.31.231/json")
+parser.add_argument('--ipaddress', dest='varIP')
+ip = parser.parse_args().varIP
+myJSON = requests.get(f"https://ipinfo.io/{ip}/json")
 myDict = json.loads(myJSON.text)
 for key in myDict:
     print(f"{key} : {myDict[key]}")
